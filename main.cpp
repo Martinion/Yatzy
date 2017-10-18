ff906e3a439a243f06d8bef17fd96e4430c0d109
Martin <pedersen.martin9@gmail.com>
2017-10-18 13:31:25 +0200

    Redone everything

--- main.cpp
+++ main.cpp
@@ -7,61 +7,33 @@ using namespace std;
 struct players
 {
     int heldDice[5]{0, 0, 0, 0, 0};
+    bool reroll[5]{1, 1, 1, 1, 1};
     int points{0};
 };
 
-void rollDies(players* player);
+void rollDice(players *player);
 
 int main()
 {
     srand(time(NULL));
 
-    players player;
+    players player1;
 
-    rollDies(&player);
+    rollDice(&player1);
 
+    for(int i = 0; i < 5; i++)
+    {
+        cout << player1.heldDice[i] << endl;
+    }
 
+    return 0;
 }
 
-void rollDies(players* player)
+void rollDice(players* player)
 {
-    int dice[6];
-
     for(int i = 0; i < 5; i++)
     {
-        dice[i] = (rand()%6) + 1;
+        if(player->reroll[i])
+            player->heldDice[i] = rand()%6 + 1;
     }
-
-    for(int i = 0; i < 5; i++)
-    {
-        cout << i << ": " << dice[i] << endl;
-    }
-
-    cout << "Which dies will you keep? choose -1 to quit" << endl;
-
-    int choice;
-
-    do
-    {
-        cin >> choice;
-
-        if(choice != -1){
-            if(dice[choice] == 0)
-                cout << "Not applicable" << endl;
-
-            int i{-1};
-
-            do
-            {
-                i++;
-
-                if(player->heldDice[i] == 0)
-                {
-                    player->heldDice[i] = dice[choice];
-                    dice[choice] = 0;
-                }
-
-            }while(dice[choice] != 0);
-        }
-    }while(choice != -1);
 }
